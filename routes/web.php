cbd52e1571261afdce4440574a6ff3cfbb8131b0
Ariel Febriansyah <arielfebriansyah.1997@gmail.com>
2024-08-12 11:35:49 +0700

    eight commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\BukuController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\LoginAdminController;
+use App\Http\Controllers\Auth\RegisterAdminController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -8,10 +11,12 @@ Route::get('/', function () {
     return Inertia::render('Home');
 });
 
-Route::get('/admin', function () {
-    return Inertia::render('Admin/AdminIndex');
-});
-
+Route::get('/registeradmin', [RegisterAdminController::class, 'index']);
+Route::post('/registeradmin', [RegisterAdminController::class, 'store']);
+Route::get('/loginadmin', [LoginAdminController::class, 'index']);
+Route::post('/loginadmin', [LoginAdminController::class, 'store']);
+Route::post('/loginadmin', [LoginAdminController::class, 'destroy'])->middleware('auth');
+Route::resource('admin', AdminController::class);
 Route::resource('bukus', BukuController::class);
 
 Route::post('/bukus/images/{buku}', [BukuController::class, 'upload']);
